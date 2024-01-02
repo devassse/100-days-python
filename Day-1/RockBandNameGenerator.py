544e0980fa7596b53803530a25d253cac0cebe41
João Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2024-01-02 09:37:12 +0200

    Day 1 Rock Band Generator

---
+++ Day-1/RockBandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Gerador automático de Nomes de Banda de Rock")
+cityWereBorn = input("Qual é o nome da Cidade onde Nasceste? ")
+petName = input("Qual é o nome do teu Primeiro animal de Estimação? ")
+
+print("Sua banda se Chamaria - ", cityWereBorn, petName)
